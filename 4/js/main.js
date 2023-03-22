fdf9aaf821c5dab2f248bf96b3d53fed6d09d6d6
Keks <keks@htmlacademy.ru>
2023-03-22 15:38:53 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,23 @@
+
+function getRandomIntInclusive(min, max)
+{
+  if (min >= 0 &&  max>= 0 && min < max)
+  {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  if (min>max)
+  {
+    return 'Incorrect order of values!';
+  }
+  {
+    return 'Negative number!';
+  }
+
+}
+
+getRandomIntInclusive(0, -2);
+
