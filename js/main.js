a1476cee3cb4ca173d52ca7e7376bbb2bca885da
Anna Titova <yboronkina@mail.ru>
2023-03-22 17:10:14 +0500

    changemodule2

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 
 function getRandomIntInclusive(min, max)
 {
-  if ( min >= 0 &&  max >= 0 && min < max)
+  if (min >= 0 &&  max>= 0 && min < max)
   {
     min = Math.ceil(min);
     max = Math.floor(max);
 
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  else
+
+  if (min>max)
   {
-    if ( min > max ) return "Incorrect order of values!";
-    else return "Negative number!";
+    return 'Incorrect order of values!';
   }
+  {
+    return 'Negative number!';
+  }
+
 }
 
-let result = getRandomIntInclusive(0, -2)
-console.log(result)
+const result = getRandomIntInclusive(0, -2);
+
